79e2fb046061d3f9e6762151c45f7282a8a5f077
maxwellworkman <email": null>
2016-06-02 10:27:37 -0700

    finished some refactoring

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,39 +1,43 @@
-class ApprenticeTeacher
+require_relative 'person'
+require_relative 'teacher_module'
+
+class ApprenticeTeacher < Person
   attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
+  include Teacher_stuff
+
   def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+    super
+    # @age = options.fetch(:age, 0)
+    # @name = options.fetch(:name, "")
     @target_raise = 800
     @phase = 3
   end
 
-  def offer_high_five
-    "High five!"
-  end
+  # def offer_high_five
+  #   "High five!"
+  # end
 
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
-  end
+  # def set_phase(num)
+  #   @phase = num
+  #   "Cool, I've always wanted to teach phase #{num}!"
+  # end
 
   def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works. "
-    response += "*drops crazy knowledge bomb* "
-    response += "... You're welcome."
+    response = super
+    response.gsub!('*drops flat-out insane', '. *drops crazy')
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
+  # def salary=(new_salary)
+  #   puts "This better be good!"
+  #   @salary = new_salary
+  # end
 
-  def receive_raise(raise)
-    @salary += raise
-  end
+  # def receive_raise(raise)
+  #   @salary += raise
+  # end
 
   def set_performance_rating(rating)
     response = ""
--- student.rb
+++ student.rb
@@ -1,16 +1,19 @@
-class Student
+require_relative 'person'
+
+class Student < Person
   attr_reader :age, :phase
   attr_accessor :name
 
   def initialize(options = {})
     @phase = 1
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+    super
+    # @age = options.fetch(:age, 0)
+    # @name = options.fetch(:name, "")
   end
 
-  def offer_high_five
-    "High five!"
-  end
+  # def offer_high_five
+  #   "High five!"
+  # end
 
   def set_phase(num)
     response = ""
